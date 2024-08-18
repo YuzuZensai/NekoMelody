6a954fa63332237cf4819352da2a851f0b193869
Yuzu <yuzu@kirameki.cafe>
2024-08-18 15:14:18 +0700

    🐛 fix: LoopMode not exposed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { Player, createPlayer } from "./player";
+import { Player, LoopMode, createPlayer } from "./player";
 import { YtDlpProvider } from "./providers";
 
 export { Player, createPlayer };
 
 export default {
     Player,
+    LoopMode,
     YtDlpProvider,
     createPlayer,
 };
