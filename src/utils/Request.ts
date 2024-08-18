0907e215b455dfdf5660e2f44e0000c102e1084b
Yuzu <yuzu@kirameki.cafe>
2024-08-18 10:10:53 +0700

    ✨ feat: Puppeteer flags

--- src/utils/Request.ts
+++ src/utils/Request.ts
@@ -58,7 +58,27 @@ export async function getStream(
 }
 
 export async function getYouTubeFormats(id: string) {
-    if (!browser) browser = await puppeteer.launch();
+    if (!browser) {
+        browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                "--disable-gpu",
+                "--disable-dev-shm-usage",
+                "--disable-setuid-sandbox",
+                "--no-first-run",
+                "--no-sandbox",
+                "--no-zygote",
+                "--deterministic-fetch",
+                "--disable-features=IsolateOrigins",
+                "--disable-site-isolation-trials",
+                "--single-process",
+            ],
+        });
+
+        browser.once("disconnected", () => {
+            browser = null;
+        });
+    }
 
     const page = await browser.newPage();
     await page.goto(`https://www.youtube.com/watch?v=${id}&has_verified=1`, {
